152edef40778a0f4f5aaf94430ec6b8fb223f17d
Allen Chan <avchan@ca.ibm.com>
2025-09-02 15:37:05 -0400

    fix lfx version

--- external_agent/examples/langflow_python/run.sh
+++ external_agent/examples/langflow_python/run.sh
@@ -10,4 +10,4 @@ else
 fi
 
 # Run the Docker container
-docker run -d -e GEMINI_API_KEY=$GEMINI_API_KEY -p 8002:8080 langflow-agent:0.1
+docker run --name travel-planner-agent -d -e GEMINI_API_KEY=$GEMINI_API_KEY -p 8002:8080 langflow-agent:0.1 
